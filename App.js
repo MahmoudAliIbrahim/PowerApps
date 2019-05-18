16fa75a56d8b5e4bdc11c1254fe288461ac51542
Ahmed A. Ibrahim <ahmed.saraya90@gmail.com>
2019-05-18 22:19:49 +0200

    use AppState to track application state

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, AppState} from 'react-native';
 import AppLink from 'react-native-app-link';
 
 type Props = {};
 export default class App extends Component<Props> {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      appState: AppState.currentState,
+    }
+  }
+
   componentDidMount() {
+    this.checkIfInstalled();
+    AppState.addEventListener('change', this._handleAppStateChange);
+  }
+
+  componentWillUnmount() {
+    AppState.removeEventListener('change', this._handleAppStateChange);
+  }
+
+  _handleAppStateChange = (nextAppState) => {
+    if (this.state.appState.match(/inactive|background/) && nextAppState === 'active') {
+      this.checkIfInstalled();
+    }
+    this.setState({appState: nextAppState});
+  };
+
+  checkIfInstalled() {
     AppLink.maybeOpenURL('ms-apps://apps', { appName: 'PowerApps', appStoreId: 'id1047318566', appStoreLocale: 'us', playStoreId: 'com.microsoft.msapps' }).then(() => {
     })
     .catch((err) => {
